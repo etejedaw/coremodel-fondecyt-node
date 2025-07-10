c4ef9421b80e7adf004cfbe675c8234e3c4a720c
Esteban Tejeda <etejedaw@hotmail.com>
2025-07-10 01:29:45 -0400

    style: add empty block in code

--- src/core/adapters/fetch-adapter/DownloadAdapter.ts
+++ src/core/adapters/fetch-adapter/DownloadAdapter.ts
@@ -4,9 +4,9 @@ import { basename } from "path";
 
 export class DownloadAdapter implements FetchAdapter {
 	async fetch(url: string): Promise<string> {
-		const res = await fetch(url);
+		const response = await fetch(url);
 
-		const buffer = Buffer.from(await res.arrayBuffer());
+		const buffer = Buffer.from(await response.arrayBuffer());
 		const fileName = basename(new URL(url).pathname) || `file-${Date.now()}`;
 		const filePath = `/tmp/${fileName}`;
 
